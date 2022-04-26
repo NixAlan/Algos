f4f7a0c5a696cfcd9fbe454afef0802d46a87bf8
Alan Nix <44075306+NixAlan@users.noreply.github.com>
2022-04-26 10:44:58 -0500

    4/26/2022

--- Fundamentals to do 4 Alan Nix/More_Accurate_Grades.js
+++ Fundamentals to do 4 Alan Nix/More_Accurate_Grades.js
@@ -10,31 +10,64 @@ function letterGrade(num) {
       console.log(`Score: ${num}. Grade: A`);
     }
   } else if (num > 79 && num < 90) {
-    console.log(`Score: ${num}. Grade: B`);
+    switch (num) {
+      case (num = 80):
+      case (num = 81):
+        console.log(`Score: ${num}. Grade: B-`);
+        break;
+      case (num = 88):
+      case (num = 89):
+        console.log(`Score: ${num}. Grade: B+`);
+        break;
+      default:
+        console.log(`Score: ${num}. Grade: B`);
+    }
   } else if (num > 69 && num < 80) {
-    console.log(`Score: ${num}. Grade: C`);
+    switch (num) {
+      case (num = 70):
+      case (num = 71):
+        console.log(`Score: ${num}. Grade: C-`);
+        break;
+      case (num = 78):
+      case (num = 79):
+        console.log(`Score: ${num}. Grade: C+`);
+        break;
+      default:
+        console.log(`Score: ${num}. Grade: C`);
+    }
   } else if (num > 59 && num < 70) {
-    console.log(`Score: ${num}. Grade: D`);
+    switch (num) {
+      case (num = 60):
+      case (num = 61):
+        console.log(`Score: ${num}. Grade: D-`);
+        break;
+      case (num = 68):
+      case (num = 69):
+        console.log(`Score: ${num}. Grade: D+`);
+        break;
+      default:
+        console.log(`Score: ${num}. Grade: D`);
+    }
   } else {
     console.log(`Score: ${num}. Grade: F`);
   }
 }
 
-letterGrade(90);
+letterGrade(78);
 
-function accurateGrade(num) {
-  console.log("devlog", num);
-  switch (num) {
-    case (num = 91):
-    case (num = 92):
-      console.log(`Score: ${num}. Grade: A-`);
-      break;
-    case {num > 9}3:
-      console.log(`Score: ${num}. Grade: A`);
-      break;
-    default:
-      console.log(`Score: ${num}. Grade: B`);
-  }
-}
+// function accurateGrade(num) {
+//   console.log("devlog", num);
+//   switch (num) {
+//     case (num = 91):
+//     case (num = 92):
+//       console.log(`Score: ${num}. Grade: A-`);
+//       break;
+//     case {num > 9}3:
+//       console.log(`Score: ${num}. Grade: A`);
+//       break;
+//     default:
+//       console.log(`Score: ${num}. Grade: B`);
+//   }
+// }
 
-accurateGrade(99);
+// accurateGrade(99);
