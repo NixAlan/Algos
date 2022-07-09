861996a20e057ec03b22c37682a9343ef13c9f2c
Nixalan <nixalan@gmail.com>
2022-07-09 08:09:47 -0500

    Claire Location

---
+++ Fundamentals to do 8/Clair_is_where.js
@@ -0,0 +1,44 @@
+//Claire is Where?
+//On New Year’s Eve, have fun but don’t forget your way home! For this challenge create four functions
+//(reset, moveBy, xLocation and yLocation) to track the travels of Claire, a wanderer. Calling reset()
+//moves Claire home to the origin (0,0). The moveBy(xOffset,yOffset) function moves her by those amounts, in those directions.
+//Finally, xLocation() and yLocation()return how far Claire is from home, in X and Y directions respectively.
+//After the calls of reset(), moveBy(1,-2), and moveBy(3,1), subsequently calling xLocation() and yLocation() should return 4 and -1.
+
+//Second: create distFromHome(). Assuming she moves diagonally, return her distance from home.
+
+class location {
+  constructor(xlocation, ylocation) {
+    this.xlocation = xlocation;
+    this.ylocation = ylocation;
+  }
+  reset() {
+    this.xlocation = 0;
+    this.ylocation = 0;
+  }
+  move(x, y) {
+    this.xlocation = this.xlocation + x;
+    this.ylocation = this.ylocation + y;
+  }
+  xlocation() {
+    return this.xlocation;
+  }
+  ylocation() {
+    return this.ylocation;
+  }
+  distFromHome() {
+    if (Math.abs(this.xlocation) > Math.abl(this.ylocation)) {
+      return Math.abs(this.xlocation);
+    } else {
+      return Math.abs(this.ylocation);
+    }
+  }
+}
+
+let claireLocation = new location(0, 0);
+claireLocation.move(1, -2);
+claireLocation.move(3, 1);
+console.log("clairs xlocation is " + claireLocation.xlocation);
+console.log("clairs ylocation is " + claireLocation.ylocation);
+claireLocation.reset();
+console.log(claireLocation.xlocation, claireLocation.ylocation);
